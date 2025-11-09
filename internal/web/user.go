e52b50f84fd0109b8e667f9c096d71d42dc8d7f7
wshadm <wangshuang6996@163.com>
2025-11-09 06:19:48 +0000

    add middleware

--- internal/web/user.go
+++ internal/web/user.go
@@ -1,42 +1,72 @@
 package web
 
 import (
+	"net/http"
+
 	regexp "github.com/dlclark/regexp2"
 	"github.com/gin-gonic/gin"
 )
-
 
 const (
 	emailRegexExpPattern = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"
 	// 和上面比起来，用 ` 看起来就比较清爽
 	passwordRegexPattern = `^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$`
 )
+
 type UserHander struct {
-	emailRegexExp *regexp.Regexp
+	emailRegexExp    *regexp.Regexp
 	passwordRegexExp *regexp.Regexp
 }
 
 func NewUserHandler() *UserHander {
 	return &UserHander{
-		emailRegexExp: regexp.MustCompile(emailRegexExpPattern, regexp.None),
+		emailRegexExp:    regexp.MustCompile(emailRegexExpPattern, regexp.None),
 		passwordRegexExp: regexp.MustCompile(passwordRegexPattern, regexp.None),
 	}
 }
 
 func (h *UserHander) RegisterRoutes(server *gin.Engine) {
-
+	ug := server.Group("/users")
+	ug.POST("/signup", h.SignUp)
+	ug.POST("/login", h.Login)
+	ug.POST("/edit", h.Edit)
+	ug.GET("/profile", h.Profile)
 }
 
 func (h *UserHander) SignUp(ctx *gin.Context) {
 	type SignUpReq struct {
-		Email string `json:"email"`
-		Password string `json:"password"`
+		Email           string `json:"email"`
+		Password        string `json:"password"`
 		ConfirmPassword string `json:"confirmPassword"`
 	}
 	var req SignUpReq
 	if err := ctx.Bind(&req); err != nil {
 		return
 	}
+
+	isEmail, err := h.emailRegexExp.MatchString(req.Email)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !isEmail {
+		ctx.String(http.StatusOK, "非法邮箱格式")
+		return
+	}
+	if req.Password != req.ConfirmPassword {
+		ctx.String(http.StatusOK, "两次驶入密码不对")
+	}
+
+	ispassword, err := h.passwordRegexExp.MatchString(req.Password)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !ispassword {
+		ctx.String(http.StatusOK, "密码必须包含字母、数字、特殊字符，并且不少于八位")
+		return
+	}
+	ctx.String(http.StatusOK, "hello, 已经注册")
 }
 
 func (h *UserHander) Login(ctx *gin.Context) {
--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/ILk8S/basc-go/internal/web"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	hdl := web.NewUserHandler()
 	server := gin.Default()
-	server.GET("/", func(context *gin.Context) {
-
+	server.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowHeaders:     []string{"Content-Type"},
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "/users")
+		},
+		MaxAge: 12 * time.Hour,
+	}), func(ctx *gin.Context) {
+		fmt.Println("这是Middleware")
 	})
-	server.Run("localhost:3000")
+	hdl.RegisterRoutes(server)
+	server.Run(":8899")
 }
