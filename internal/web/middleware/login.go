047cfe17026d679477c1eed2abf6e2396f8bdd45
wshadm <wangshuang6996@163.com>
2025-11-15 16:15:22 +0000

    JWT + 增加安全相关

--- internal/web/middleware/login.go
+++ internal/web/middleware/login.go
@@ -1,7 +1,10 @@
 package middleware
 
 import (
+	"encoding/gob"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -11,6 +14,7 @@ type LoginMiddlewareBuilder struct {
 }
 
 func (m *LoginMiddlewareBuilder) CheckLogin() gin.HandlerFunc {
+	gob.Register(time.Now())
 	return func(ctx *gin.Context) {
 		path := ctx.Request.URL.Path
 		if path == "/users/signup" || path == "/users/login" {
@@ -18,10 +22,25 @@ func (m *LoginMiddlewareBuilder) CheckLogin() gin.HandlerFunc {
 			return
 		}
 		sess := sessions.Default(ctx)
-		if sess.Get("userId") == nil {
+		userId := sess.Get("userId")
+		if userId == nil {
 			// 中断，不要往后执行，也就是不要执行后面的业务逻辑
 			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
+		}
+		now := time.Now()
+		const updateTimeKey = "update_time"
+		//拿出上一次的刷新时间
+		val := sess.Get(updateTimeKey)
+		lastUpdateTime, ok := val.(time.Time)
+		if val == nil || !ok || now.Sub(lastUpdateTime) > time.Second*10 {
+			// 如果到这里说明没刷新过，或者还没到刷新的时间
+			sess.Set(updateTimeKey, now)
+			sess.Set("userId", userId)
+			err := sess.Save()
+			if err != nil {
+				fmt.Errorf("session save is error: %w", err)
+			}
 		}
 	}
 }
--- main.go
+++ main.go
@@ -10,10 +10,12 @@ import (
 	"github.com/ILk8S/basc-go/internal/service"
 	"github.com/ILk8S/basc-go/internal/web"
 	"github.com/ILk8S/basc-go/internal/web/middleware"
+	"github.com/ILk8S/basc-go/pkg/ginx/middleware/ratelimit"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -25,16 +27,8 @@ func main() {
 	server.Run(":8899")
 }
 
-func initUserHdl(db *gorm.DB, server *gin.Engine) {
-	ud := dao.NewUserDAO(db)
-	ur := repository.NewUserRepository(ud)
-	us := service.NewUserService(ur)
-	hdl := web.NewUserHandler(us)
-	hdl.RegisterRoutes(server)
-}
-
 func initDB() *gorm.DB {
-	db, err := gorm.Open(mysql.Open("root:123456@tcp(localhost:3306)/webook"))
+	db, err := gorm.Open(mysql.Open("root:123456@tcp(192.168.1.40:3306)/webook"))
 	if err != nil {
 		panic(err)
 	}
@@ -49,7 +43,9 @@ func initWebServer() *gin.Engine {
 	server := gin.Default()
 	server.Use(cors.New(cors.Config{
 		AllowCredentials: true,
-		AllowHeaders:     []string{"Content-Type"},
+		//AllowHeaders:     []string{"Content-Type"},
+		AllowHeaders:  []string{"Content-Type", "Authorization"},
+		ExposeHeaders: []string{"x-jwt-token"},
 		AllowOriginFunc: func(origin string) bool {
 			if strings.HasPrefix(origin, "http://localhost") {
 				return true
@@ -58,12 +54,39 @@ func initWebServer() *gin.Engine {
 		},
 		MaxAge: 12 * time.Hour,
 	}), func(ctx *gin.Context) {
-		fmt.Println("这是Middleware")
+		fmt.Println("Middleware")
 	})
-	login := &middleware.LoginMiddlewareBuilder{}
-	// 存储数据的，也就是你 userId 存哪里
-	// 直接存 cookie
-	store := cookie.NewStore([]byte("secret"))
-	server.Use(sessions.Sessions("ssid", store), login.CheckLogin())
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "192.168.1.40:6379",
+		Password: "pA3L48JS",
+	})
+	//限制每秒最多1个请求
+	server.Use(ratelimit.NewBuilder(redisClient, time.Second, 1).Build())
+	useJWT(server)
 	return server
+}
+
+func useJWT(server *gin.Engine) {
+	login := &middleware.LoginJWTMiddlewareBuilder{}
+	server.Use(login.CheckLogin())
+}
+
+func useSession(server *gin.Engine) {
+	login := &middleware.LoginMiddlewareBuilder{}
+	store := cookie.NewStore([]byte("secret"))
+	//store, err := redis.NewStore(16, "tcp",
+	//	"192.168.1.40:6379", "",
+	//	"pA3L48JS", []byte("JXMYyZfJI9kFrzq0ggoHmSqE6UAUuW8L"))
+	//if err != nil {
+	//	panic(errors.New("redis connection faild"))
+	//}
+	server.Use(sessions.Sessions("ssid", store), login.CheckLogin())
+}
+
+func initUserHdl(db *gorm.DB, server *gin.Engine) {
+	ud := dao.NewUserDAO(db)
+	ur := repository.NewUserRepository(ud)
+	us := service.NewUserService(ur)
+	hdl := web.NewUserHandler(us)
+	hdl.RegisterRoutes(server)
 }
